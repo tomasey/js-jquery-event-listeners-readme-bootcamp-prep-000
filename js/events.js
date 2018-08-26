4120ee43283d85e318095634f0cd448dddabec9c
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-26 22:25:44 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
-//define functions here
+function getIt () {
+  $('p').on('click', alert("Hey!"));
+}
+
+function frameIt () {
+  $('img').addClass("tasty");
+  $('#tasty').on('load').css('border', 'solid red');
+}
+
+function pressIt () {
+  $('form input').on('keydown', function G (key) {
+      if (key.which === 71) {
+      alert("You have pressed the G key!");
+      } 
+                                })
+}
+
+function submitIt () {
+  $('form').on('submit', alert("Your form is going to be submitted now."));
+}
+   
+
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
